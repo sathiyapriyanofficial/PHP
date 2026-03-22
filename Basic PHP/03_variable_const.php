b24c6c32355a6e988461d79beb9be874cb268535
Sathiya Priyan <sathiyapriyan.official@gmail.com>
2026-03-22 20:16:07 +0000

    constants in procedural PHP

---
+++ Basic PHP/03_variable_const.php
@@ -0,0 +1,13 @@
+<?php
+
+$first_name = 'steve';
+$last_name = 'jobs';
+
+// Define a const in two ways 
+
+// DEFINE 
+define('name', 'value');
+define('STATUS_PAID', 'paid');
+echo STATUS_PAID;
+
+// const keyword

d4cdfe6f9fac04c06440c15a0793238203611e3a
Sathiya Priyan <sathiyapriyan.official@gmail.com>
2026-04-06 21:18:56 +0100

    unqualified class name like name resolve

--- OOP/src/PaymentGateway/Stripe/Transaction.php
+++ OOP/src/PaymentGateway/Stripe/Transaction.php
@@ -4,4 +4,15 @@ declare(strict_types=1);
 
 namespace PaymentGateway\Stripe;
 
-class Transaction {}
+class Transaction
+{
+  public function __construct()
+  {
+    var_dump(\explode(',', 'HelloWorld'));
+  }
+}
+
+function explode($separator, $str)
+{
+  return 'foo';
+}

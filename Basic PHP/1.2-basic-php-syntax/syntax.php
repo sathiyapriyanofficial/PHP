ccc6927b64a0a05691105907d8b53d40bebbd293
Sathiya Priyan <sathiyapriyan.official@gmail.com>
2026-05-02 19:05:14 +0100

    basic PHP syntax

---
+++ Basic PHP/1.2-basic-php-syntax/syntax.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+
+echo "Hello, World!" . PHP_EOL;
+echo "Hello, World! \n";
+
+// Single quote
+echo 'Hello, World!' . PHP_EOL;
+
+$name = "John";
+echo "Hello, $name!" . PHP_EOL; 
